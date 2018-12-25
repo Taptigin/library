56e16bb83533fbb6fbfc56da900055757226887c
Seliverstow Alexandr <a.v.seliverstow@gmail.com>
2018-12-25 19:19:16 +0400

    add first service

--- src/main/java/ru/me/Controllers/LibraryController.java
+++ src/main/java/ru/me/Controllers/LibraryController.java
@@ -1,15 +1,33 @@
 package ru.me.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.me.Services.LibraryService;
+import ru.me.models.Author;
 
 @RestController
 @RequestMapping("/library")
 public class LibraryController {
+    private ApplicationContext context = new ClassPathXmlApplicationContext("SpringContext.xml");
+    private LibraryService libraryService = (LibraryService) context.getBean("storageService");
+   // private LibraryService libraryService;
 
-    private LibraryService libraryService;
 
+    @Autowired
+    public void setLibraryService(LibraryService libraryService) {
+        this.libraryService = libraryService;
+    }
 
+    public static void main(String[] args) {
+        new LibraryController().start();
+    }
+
+    void start(){
+        Author author =  libraryService.getAuthorByName("Тестовый автор 1");
+        System.out.println(author.getName());
+    }
 
 }
