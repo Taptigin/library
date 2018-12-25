dfff10a09413439fa8e3dc02d49f5aefb04833a3
Seliverstow Alexandr <a.v.seliverstow@gmail.com>
2018-12-25 17:58:56 +0400

    create Author Entity

---
+++ src/main/java/ru/me/models/Author.java
@@ -0,0 +1,57 @@
+package ru.me.models;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.Objects;
+
+/**
+ * Created by Александр on 25.12.2018.
+ */
+@Entity
+@Table(name = "author")
+public class Author {
+    private long id;
+    private String name;
+    private final static String SEQUENCE_NAME = "authorSeq";
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(generator = SEQUENCE_NAME, strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME, allocationSize = 1)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", nullable = true, length = 100)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id &&
+                Objects.equals(name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
