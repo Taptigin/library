1c865dfe70c7a39e65b971fedb986a065d6d5baa
Aleksandr_Seliversto <Aleksandr_Seliverstov@epam.com>
2019-01-17 14:34:54 +0400

    fix

--- SqlScripts.sql
+++ SqlScripts.sql
@@ -138,7 +138,7 @@ insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED)
 values (2, 'dbuser1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', 1);
 
 insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED)
-values (1, 'dbadmin1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', 1);
+values (1, 'admin', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', 1);
 
 ---
 
